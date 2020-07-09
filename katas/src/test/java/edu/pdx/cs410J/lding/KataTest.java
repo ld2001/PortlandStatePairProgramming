89ddfaca2c5125b70e3006216843d5018781d5f4
Luke Ding <lding@pdx.edu>
2020-07-08 19:56:23 -0700

    First Push

---
+++ katas/src/test/java/edu/pdx/cs410J/lding/KataTest.java
@@ -0,0 +1,29 @@
+package edu.pdx.cs410J.lding;
+
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class KataTest
+{
+
+  @Test
+  public void canInstantiateKataClass() {
+    new Kata();
+  }
+
+  @Test
+
+
+
+}
